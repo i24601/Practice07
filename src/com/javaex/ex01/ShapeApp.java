6053c88df763041834b460884bd8c3cb0e443fb3
i24601 <sgs1044@naver.com>
2020-05-22 14:46:47 +0900

    practice ex01 fix

--- src/com/javaex/ex01/ShapeApp.java
+++ src/com/javaex/ex01/ShapeApp.java
@@ -6,7 +6,7 @@ public class ShapeApp {
 
 //		Shape s = new Shape("빨강");
 		//추상클래스는 객체생성 할수 없다
-		//추상클래스를 상속받는 Rectangle Cirlcle을 통해 추상클래스 인스턴스인 필드값(fillColor)에 접근해야한다
+		//추상클래스를 상속받는 Rectangle Cirlcle을 통해 추상클래스 필드값(fillColor)에 접근해야한다
 		//c1.fillColor="빨강" 또는 r1.fillColor
 		Circle c1 = new Circle("녹색", 10);
 		Ractangle r1 = new Ractangle("빨강", 4, 4);
@@ -19,7 +19,7 @@ public class ShapeApp {
 		sr1.draw();
 		//sr1의 가로크기 출력
 		System.out.printf("sr1의 가로는 %d 입니다",((Ractangle)sr1).getWidth());
-		//Shape형인 sr1을 Ractangle형으로 형변환
+		//Shape형인 sr1을 Ractangle형으로 캐스팅
 		//sr1의 width는 private이므로 getWidth를 통해 접근
 	}
 }
